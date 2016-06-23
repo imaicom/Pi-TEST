3f81b8634a83b1e8cc7ec29567de6951b8c40b79
makoto imai <i@okako.com>
2016-06-23 09:06:57 +0900

    modified:   mytest 	modified:   mytest.c

--- mytest.c
+++ mytest.c
@@ -5,17 +5,20 @@
 
 int main() {
   
-  unsigned char sw;
+  unsigned char sen1,sen2;
   
   wiringPiSetup();
   
   pinMode(15,INPUT);
   pullUpDnControl(15,PUD_UP);
+  pinMode(16,INPUT);
+  pullUpDnControl(16,PUD_UP);
   printf("Raspberry Pi SW & LED\n");
 
   while (1) {
-    sw = digitalRead(15);
-    printf("SWIN:%d\n",sw);
+    sen1 = digitalRead(15);
+    sen2 = digitalRead(16);
+    printf("SWIN:%d %d\n",sen1,sen2);
     delay(500);
   }
   return 0;
