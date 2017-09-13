e7f87e889dbfc66e42b017cfd9a6ed1c847b4a98
makoto imai <i@okako.com>
2017-09-13 14:16:49 +0900

    new file:   3sw-led.c

---
+++ 3sw-led.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <wiringPi.h>
+
+void main() {
+	
+	wiringPiSetup();
+	
+	pinMode(30,INPUT);	// GREEN  SW
+	pinMode(21,INPUT);	// YELLOW SW
+	pinMode(22,INPUT);	// RED    SW
+	
+	pinMode(23,OUTPUT);	// GREEN  LED
+	pinMode(24,OUTPUT);	// YELLOW LED
+	pinMode(25,OUTPUT);	// RED    LED
+   
+    while(1) {
+		digitalWrite(23 , digitalRead(30)); // GREEN  TEST
+		digitalWrite(24 , digitalRead(21)); // YELLOW TEST
+		digitalWrite(25 , digitalRead(22)); // RED    TEST
+	};
+}
+
