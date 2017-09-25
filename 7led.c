d7c0bfdb18d0787d7c92da1c338a29a75dbb9250
makoto imai <i@okako.com>
2017-09-25 12:31:49 +0900

    new file:   7led 	new file:   7led.c

---
+++ 7led.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <wiringPi.h>
+
+main() {
+	
+	wiringPiSetup();
+	
+	
+	pinMode( 2,OUTPUT);	// a
+	pinMode( 0,OUTPUT);	// b
+	pinMode(22,OUTPUT);	// c
+	pinMode(23,OUTPUT);	// d
+	pinMode(24,OUTPUT);	// e
+	pinMode( 3,OUTPUT);	// f
+	pinMode(14,OUTPUT);	// g
+	pinMode(21,OUTPUT);	// h
+	pinMode(13,OUTPUT);	// Left  K
+	pinMode(12,OUTPUT);	// Right K
+   
+	digitalWrite( 2 , 1); // a
+	digitalWrite( 0 , 1); // b
+	digitalWrite(22 , 1); // c
+	digitalWrite(23 , 1); // d
+	digitalWrite(24 , 1); // e
+	digitalWrite( 3 , 1); // f
+	digitalWrite(14 , 1); // g
+	digitalWrite(21 , 1); // h
+	digitalWrite(13 , 1); // Left  K
+	digitalWrite(12 , 1); // Right K
+}
