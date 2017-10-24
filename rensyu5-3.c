68dacff9d490b3915ecc7c8dc88553d221287420
KURURU <i@okako.com>
2017-10-24 15:47:35 +0900

    1

--- rensyu5-3.c
+++ rensyu5-3.c
@@ -1,14 +1,14 @@
 /* 練習問題 5-3 */
 #include <stdio.h>
 int main(void){
-char a,b,c,d;
-  a = 0x54;
-  b = 0x72;
-  c = 0x6f;
-  d = 0x75;
-  printf("%c",a);
-  printf("%c",b);
-  printf("%c",c);
-  printf("%c\n",d);
-  return 0;
+	char a,b,c,d;
+	a = 0x54;
+	b = 0x72;
+	c = 0x6f;
+	d = 0x75;
+	printf("%c",a);
+	printf("%c",b);
+	printf("%c",c);
+	printf("%c\n",d);
+	return 0;
 }
