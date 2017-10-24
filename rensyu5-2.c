80ab2c7d10c31a0b3d5e06c3da38b65179762549
KURURU <i@okako.com>
2017-10-24 15:46:22 +0900

    1

--- rensyu5-2.c
+++ rensyu5-2.c
@@ -1,12 +1,12 @@
 /* 練習問題 5-2 */
 #include <stdio.h>
 int main(void){
-  float a,b,keisan1,keisan2;
-  a = 35.2;
-  b = 85.6;
-  keisan1 = a * a + b * b;
-  keisan2 = a * a - b * b;
-  printf("a * a + b * b = %f\n",keisan1);
-  printf("a * a - b * b = %f\n",keisan2);
-  return 0;
+	float a,b,keisan1,keisan2;
+	a = 35.2;
+	b = 85.6;
+	keisan1 = a * a + b * b;
+	keisan2 = a * a - b * b;
+	printf("a * a + b * b = %f\n",keisan1);
+	printf("a * a - b * b = %f\n",keisan2);
+	return 0;
 }
