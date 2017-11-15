67171b11ffd76978fb7a0e91572fd8cd0f32d1eb
makoto imai <i@okako.com>
2017-11-15 10:49:54 +0900

    new file:   7led-2  new file:   7led-2.c

---
+++ 7led-2.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <wiringPi.h>
+
+main() {
+	
+	wiringPiSetup();
+	
+	pinMode( 2,OUTPUT);	// a
+	pinMode( 0,OUTPUT);	// b
+	pinMode(22,OUTPUT);	// c
+	pinMode(23,OUTPUT);	// d
+	pinMode(24,OUTPUT);	// e
+	pinMode( 3,OUTPUT);	// f
+	pinMode(14,OUTPUT);	// g
+	pinMode(21,OUTPUT);	// h
+	pinMode(13,OUTPUT);	// Left  K
+	pinMode(12,OUTPUT);	// Right K
+   
+	while(1) {
+		digitalWrite( 2 ,1); // a
+		digitalWrite( 0 ,1); // b
+		digitalWrite(22 ,1); // c
+		digitalWrite(23 ,1); // d
+		digitalWrite(24 ,1); // e
+		digitalWrite( 3 ,1); // f
+		digitalWrite(14 ,0); // g
+		digitalWrite(21 ,0); // h
+		
+		digitalWrite(13 ,0); // Left  K
+		digitalWrite(12 ,1); // Right K
+		delay(1000);
+		
+		digitalWrite( 2 ,0); // a
+		digitalWrite( 0 ,1); // b
+		digitalWrite(22 ,1); // c
+		digitalWrite(23 ,0); // d
+		digitalWrite(24 ,0); // e
+		digitalWrite( 3 ,0); // f
+		digitalWrite(14 ,0); // g
+		digitalWrite(21 ,0); // h
+		
+		digitalWrite(13 ,0); // Left  K
+		digitalWrite(12 ,1); // Right K
+		delay(1000);
+	}
+}
+
